4723e5071157c1e08c4d3abdd53c505505c10421
yc <kixi@163.com>
2020-05-05 10:28:26 +0800

    文档调整

--- pages/checklist/index.js
+++ pages/checklist/index.js
@@ -8,34 +8,17 @@ Pager({
   data: {
     checkListConfig: mkCheckList({
       id: 'xxx',
-      checkedType: 1,
-      value: ['3'],
+      checkedType: 2,
+      isSwitch: true,
+      value: ['333'],
       data: [
-        {title: 'aaa', value: '1', idf: 'aaa'},
-        {title: '你好', value: '1-1', parent: 'aaa'},
-        {title: '你妹', value: '1-2', parent: 'aaa'},
-
-        {title: [
-          {"@icon": {type: 'success', size: '24', style: 'margin: 4px 10px 0 0'},}, 
-          'bbb'
-        ], value: '2', idf: 'bbb'},
-        {title: 'bbb-1', value: 'bbb-1', parent: 'bbb'},
-        {title: 'bbb-2', value: 'bbb-2', parent: 'bbb'},
-        {title: 'bbb-3', value: 'bbb-3', parent: 'bbb'},
-        {title: 'bbb-4', value: 'bbb-4', parent: 'bbb'},
-        {title: 'bbb-5', value: 'bbb-5', parent: 'bbb'},
-
-        
-        {title: 'ccc', value: '3', idf: 'ccc', checkListOption: {value: ['3-1'], selectAll: true}},
-        { title: 'ccc-1', value: '3-1', idf: 'ccc-1', parent: 'ccc', checkListOption: { value: ['4-1'], checkedType: 2, selectAll: true}},
-        {title: 'ccc-1-1', value: '4-1', parent: 'ccc-1'},
-        {title: 'ccc-1-2', value: '4-2', parent: 'ccc-1'},
-        {title: 'ccc-1-3', value: '4-3', parent: 'ccc-1'},
-
-        {title: 'ccc-2', value: '3-2', idf: 'ccc-2', parent: 'ccc', checkListOption: {checkedType: 2}},
-        {title: 'ccc-2-1', value: '5-1', parent: 'ccc-2'},
-        {title: 'ccc-2-2', value: '5-2', parent: 'ccc-2'},
-        {title: 'ccc-2-3', value: '5-3', parent: 'ccc-2'},
+        {title: '222', value: '222'},
+        {title: '333', value: '333'},
+        {title: '444', value: '444'},
+        '========',
+        {title: '666', value: '666'},
+        {title: '777', value: '777'},
+        {title: '888', value: '888'},
       ]
     })
   },
@@ -45,14 +28,5 @@ Pager({
     xxx.tap = function(allv) {
       console.log('=======0000', allv);
     }
-    // setTimeout(() => {
-    //   xxx.clear()
-    // }, 2000);
-
-    // setTimeout(() => {
-    //   console.log('====== kkkk');
-    //   xxx.clear('1')
-    //   console.log(xxx.getValue());
-    // }, 5000);
   }
 })
