1f4b0422d40e8e7943ef7bc4803e7abd469319ef
yc <kixi@163.com>
2021-10-23 01:22:17 +0800

    删除冗余组件

--- js/components/calendar/helper/util/formatdate.js
+++
@@ -1,6 +0,0 @@
-import { getYmd } from "./getymd";
-
-export function formatDate(date) {
-  let ymd = getYmd(date)
-  return `${ymd.year}-${ymd.month}-${ymd.day}`
-}
--- js/components/calendar/helper/util/isleapyear.js
+++
@@ -1,5 +0,0 @@
-// 工具方法 - start
-// 1.为了获得每个月的日期有多少，我们需要判断 平年闰年[四年一闰，百年不闰，四百年再闰]
-export function isLeapYear(year) {
-  return (year % 400 === 0) || (year % 100 !== 0 && year % 4 === 0);
-}
