5d5160d849f4102038a36022e7f86a62b172796d
yc <kixi@163.com>
2022-02-17 01:51:41 +0800

    新增echarts图表demo

---
+++ js/pages/echarts/index.js
@@ -0,0 +1,133 @@
+//index.js
+//获取应用实例
+const app = getApp()
+const Pager = require('../../components/aotoo/core/index')
+let source = require('../common/source')
+
+function asyncInit(context, echarts){
+  return function initChart(canvas, width, height, dpr) {
+    const chart = echarts.init(canvas, null, {
+      width: width,
+      height: height,
+      devicePixelRatio: dpr // new
+    });
+    canvas.setChart(chart);
+
+    let option = {
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+          type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+        },
+        confine: true
+      },
+      legend: {
+        data: ['热度', '正面', '负面']
+      },
+      grid: {
+        left: 20,
+        right: 20,
+        bottom: 15,
+        top: 40,
+        containLabel: true
+      },
+      xAxis: [
+        {
+          type: 'value',
+          axisLine: {
+            lineStyle: {
+              color: '#999'
+            }
+          },
+          axisLabel: {
+            color: '#666'
+          }
+        }
+      ],
+      yAxis: [
+        {
+          type: 'category',
+          axisTick: { show: false },
+          data: ['汽车之家', '今日头条', '百度贴吧', '一点资讯', '微信', '微博', '知乎'],
+          axisLine: {
+            lineStyle: {
+              color: '#999'
+            }
+          },
+          axisLabel: {
+            color: '#666'
+          }
+        }
+      ],
+      series: [
+        {
+          name: '热度',
+          type: 'bar',
+          label: {
+            normal: {
+              show: true,
+              position: 'inside'
+            }
+          },
+          data: [300, 270, 340, 344, 300, 320, 310],
+          itemStyle: {
+            // emphasis: {
+            //   color: '#37a2da'
+            // }
+          }
+        },
+        {
+          name: '正面',
+          type: 'bar',
+          stack: '总量',
+          label: {
+            normal: {
+              show: true
+            }
+          },
+          data: [120, 102, 141, 174, 190, 250, 220],
+          itemStyle: {
+            // emphasis: {
+            //   color: '#32c5e9'
+            // }
+          }
+        },
+        {
+          name: '负面',
+          type: 'bar',
+          stack: '总量',
+          label: {
+            normal: {
+              show: true,
+              position: 'left'
+            }
+          },
+          data: [-20, -32, -21, -34, -90, -130, -110],
+          itemStyle: {
+            // emphasis: {
+            //   color: '#67e0e3'
+            // }
+          }
+        }
+      ]
+    };
+
+    chart.setOption(option);
+    context.chart = chart
+    return chart;
+  }
+}
+
+Pager({
+  data: {
+    ec: null,
+    ...source
+  },
+  async onLoad(){
+    require('../../ec-canvas/echarts.js', echarts => {
+      this.setData({ec: {
+        onInit: asyncInit(this, echarts)
+      }})
+    })
+  }
+})
